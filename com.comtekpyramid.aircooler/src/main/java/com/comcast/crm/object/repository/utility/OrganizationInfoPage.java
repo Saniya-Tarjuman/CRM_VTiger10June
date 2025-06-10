a7a76a3205805c0bfcbab4cfbfe607953eb2637d
Tushar <Tushar@Tushar>
2025-06-10 10:54:08 +0530

    Organization module POM pages added

---
+++ com.comtekpyramid.aircooler/src/main/java/com/comcast/crm/object/repository/utility/OrganizationInfoPage.java
@@ -0,0 +1,41 @@
+package com.comcast.crm.object.repository.utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrganizationInfoPage {
+	public OrganizationInfoPage(WebDriver driver) {
+		PageFactory.initElements(driver,this);
+	}
+	public WebElement getOrganizationNameTextFiled() {
+		return organizationNameTextFiled;
+	}
+	public WebElement getIndustryDropDown() {
+		return industryDropDown;
+	}
+	public WebElement getTypeDropDown() {
+		return typeDropDown;
+	}
+	public WebElement getSaveBtn() {
+		return saveBtn;
+	}
+	public WebElement getPhoneNumberTextField() {
+		return phoneNumberTextField;
+	}
+	@FindBy(name = "accountname")
+	private WebElement organizationNameTextFiled;
+	@FindBy(name = "industry")
+	private WebElement industryDropDown;
+	@FindBy(name = "accounttype")
+	private WebElement typeDropDown;
+	@FindBy(xpath = "//input[@title='Save [Alt+S]']")
+	private WebElement saveBtn;
+	@FindBy(id = "phone")
+	private WebElement phoneNumberTextField;
+	
+
+
+
+}
