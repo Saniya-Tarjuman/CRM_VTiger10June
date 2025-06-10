69655e362c07ddaf0bec2933cefc284ad5e82935
Tushar <Tushar@Tushar>
2025-06-10 12:24:47 +0530

    modified pom.xml and OrganizationCreatePage

--- com.comtekpyramid.aircooler/src/main/java/com/comcast/crm/object/repository/utility/OrganizationCreatedPage.java
+++ com.comtekpyramid.aircooler/src/main/java/com/comcast/crm/object/repository/utility/OrganizationCreatedPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 /**
-@author Tushar:
+@author Tushar
 Elements of OrganizationCreatedPage
 **/
 public class OrganizationCreatedPage {
