67e481a5e8dec5b374c66fc2b63e16db51b2d448
Admin <Admin@DESKTOP-BDETOOS>
2025-06-10 12:16:58 +0530

    Added the changes

--- com.comtekpyramid.aircooler/src/main/java/com/comcast/crm/object/repository/utility/ContactPage.java
+++ com.comtekpyramid.aircooler/src/main/java/com/comcast/crm/object/repository/utility/ContactPage.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+/**
+ * @author Admin
+ */
 public class ContactPage {
 	WebDriver driver;
     public ContactPage(WebDriver driver) {
